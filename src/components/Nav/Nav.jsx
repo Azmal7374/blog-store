666b78c66550b4d072bbf53b14e16493b45d9455
Azmal <azmalgazi8@gmail.com>
2023-03-30 21:02:43 +0600

    added responsive navbar done

--- src/components/Nav/Nav.jsx
+++ src/components/Nav/Nav.jsx
@@ -3,16 +3,14 @@ import React from 'react';
 const Nav = () => {
     return (
         <div>
-        <div className="navbar w-100% h-30" style={{backgroundColor: '#1C2B35'}}>
+        <div className="navbar w-100% h-30 md:p-8" style={{backgroundColor: '#1C2B35'}}>
         <div className="flex-1">
-          
-        <img className='' src="" alt="" />
+        <h2 className="md:text-4xl text-white md:px-5 font-bold">Knowledge Cafe</h2>
         </div>
         <div className="flex-none">
-          <ul className="menu menu-horizontal px-3 text-white font-semibold">
-            <li><a>Shop</a></li>
-            <li><a>Order</a></li>
-             <li><a>Inventory</a></li>
+          <ul className="menu menu-horizontal md:px-10 text-white font-semibold md:text-2xl">
+            <li><a>Blogs</a></li>
+            <li><a>Contact</a></li>
             <li><a>Login</a></li>
           </ul>
         </div>
